6225bc27f6c2dd0d39437faeb5ec5319b0290b73
Priyadharshini R <144543680+priya-dharshini1207@users.noreply.github.com>
2026-03-13 14:41:22 +0530

    Create practice makes us perfect.java

---
+++ practice makes us perfect.java
@@ -0,0 +1,23 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner scan=new Scanner(System.in);
+		int count=0;
+		for(int i=0;i<4;i++)
+		{
+		    int p=scan.nextInt();
+		    if(p>=10)
+		    {
+		       count++; 
+		    }
+		    
+		}
+		System.out.println(count);
+
+	}
+}
