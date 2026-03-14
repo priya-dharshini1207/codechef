d51b58a009cf2d7983add4fd743de3ee45d38cfe
Priyadharshini R <144543680+priya-dharshini1207@users.noreply.github.com>
2026-03-14 14:31:46 +0530

    Create new year greeting.java

---
+++ new year greeting.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner scan=new Scanner(System.in);
+		if(scan.hasNextInt()){
+		    int X=scan.nextInt();
+		    int hoursleft=24-X;
+		    System.out.println(hoursleft);
+		    
+		}
+
+	}
+}
