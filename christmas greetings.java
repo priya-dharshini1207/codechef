c68471f17e3baed115d11a6813658704c2539ed0
Priyadharshini R <144543680+priya-dharshini1207@users.noreply.github.com>
2026-03-14 12:28:54 +0530

    Create christmas greetings.java

---
+++ christmas greetings.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	    Scanner scan=new Scanner(System.in);
+	    if(scan.hasNextInt()){
+	        int x=scan.nextInt();
+	        System.out.println(x == 25 ? "CHRISTMAS" : "ORDINARY");
+	    }
+
+	}
+}
