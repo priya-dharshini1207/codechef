8d596e176c7df54fe6bf8fd551174ee48de0d96d
Priyadharshini R <144543680+priya-dharshini1207@users.noreply.github.com>
2026-03-14 14:36:25 +0530

    Create access code equality.java

---
+++ access code equality.java
@@ -0,0 +1,26 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+    public static void main (String[] args) throws java.lang.Exception
+    {
+        
+        Scanner sc = new Scanner(System.in);
+        
+     
+        if (sc.hasNext()) {
+            String s = sc.next();
+            
+     
+            if (s.equals("WECNITK")) {
+                System.out.println("Welcome to Web Club!");
+            } else {
+                System.out.println("Access denied");
+            }
+        }
+        
+        sc.close();
+    }
+}
